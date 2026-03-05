983d20d20a480f6595cf8a4123ecabd2d01ee50f
Guilherme Pejon <guilherme.pejon@housecallpro.com>
2026-03-05 19:31:36 -0300

    Improved oauth login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      remember_me(@user)
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
